7a664acd60dabdfb81ceb2b2e49105b81bd6c7f5
Steven Harper <20556807+shweshipu@users.noreply.github.com>
2023-09-28 16:48:57 +0000

    add flip card

--- scripts.js
+++ scripts.js
@@ -1,1 +1,13 @@
-console.log('hello worldadwdawdawdaw')
+console.log('hello worldadwdawdawdaw');
+// Check JS is Connected
+console.log("👍 JS Connected");
+
+// get all the card
+document.querySelectorAll(".card").forEach((item) => {
+  // for each item i.e. card, do this
+  item.addEventListener("click", (event) => {
+    // toggle class on card
+    console.log("flipped");
+    item.classList.toggle("flipped");
+  });
+});
